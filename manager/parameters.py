f2660a157864a13d2d9dc501a48cdc7f69d97c5a
zcajiayin <cong.zhang92@gmail.com>
2021-08-25 14:54:02 +0800

    change_iteration_for_continueing_train

--- manager/parameters.py
+++ manager/parameters.py
@@ -6,7 +6,7 @@ parser.add_argument('--n_agent', type=int, default=5)
 parser.add_argument('--n_nodes', type=int, default=150)
 parser.add_argument('--batch_size', type=int, default=128)
 parser.add_argument('--lr', type=float, default=1e-4)
-parser.add_argument('--iteration', type=int, default=6000)
+parser.add_argument('--iteration', type=int, default=5000)
 parser.add_argument('--sh_or_mh', type=str, default='MH')  # 'MH'-MultiHead, 'SH'-SingleHead
 parser.add_argument('--node_embedding_type', type=str, default='gin')  # 'mlp', 'gin'
 parser.add_argument('--hidden_dim', type=int, default=32)
