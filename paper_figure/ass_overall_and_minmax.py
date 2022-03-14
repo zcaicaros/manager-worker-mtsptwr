b8b45783784e026851c11602c2d4f2486bf1d987
unknown <cong.zhang92@gmail.com>
2022-03-14 16:25:01 +0800

    general update

--- paper_figure/ass_overall_and_minmax.py
+++ paper_figure/ass_overall_and_minmax.py
@@ -7,7 +7,7 @@ import matplotlib as mpl
 
 
 n = 100
-m = 5
+m = 10
 
 min1 = np.load('../manager/counts/count_min_{}_{}_overall.npy'.format(n, m))
 mean1 = np.load('../manager/counts/count_mean_{}_{}_overall.npy'.format(n, m))
@@ -19,12 +19,12 @@ max2 = np.load('../manager/counts/count_max_{}_{}_minmax.npy'.format(n, m))
 fig, ax = plt.subplots(figsize=(16, 11.6))
 ax.set_xlim(-3, 103)
 x = np.arange(1, 101)
-l4 = plt.plot(x, max2, '-', label='minmax (n={}, m={}):max'.format(n, m), linewidth=3, color='coral', ms=9)
-l3 = plt.plot(x, min2, '--', label='minmax (n={}, m={}):min'.format(n, m), linewidth=3, color='coral', ms=9)
-l5 = plt.plot(x, mean2, '--', label='', linewidth=1, color='red', ms=9)
-l2 = plt.plot(x, max1, '-', label='overall (n={}, m={}):max'.format(n, m), linewidth=3, color='royalblue', ms=9)
-l1 = plt.plot(x, min1, '--', label='overall (n={}, m={}):min'.format(n, m), linewidth=3, color='royalblue', ms=9)
-l6 = plt.plot(x, mean1, '-', label='', linewidth=1, color='blue', ms=9)
+l4 = plt.plot(x, max2, '-', label='minmax: max assignment', linewidth=3, color='coral', ms=9)
+l3 = plt.plot(x, min2, '--', label='minmax: min assignment', linewidth=3, color='coral', ms=9)
+# l5 = plt.plot(x, mean2, '--', label='', linewidth=1, color='red', ms=9)
+l2 = plt.plot(x, max1, '-', label='overall: max assignment', linewidth=3, color='royalblue', ms=9)
+l1 = plt.plot(x, min1, '--', label='overall: min assignment', linewidth=3, color='royalblue', ms=9)
+# l6 = plt.plot(x, mean1, '-', label='', linewidth=1, color='blue', ms=9)
 
 plt.fill_between(x, max1, min1, color='cornflowerblue', alpha=0.5, linewidth=0)
 plt.fill_between(x, max2, min2, color='darksalmon', alpha=0.5, linewidth=0)
@@ -38,5 +38,5 @@ plt.ylabel('Number of Assigned Customers', {'family': 'Times New Roman', 'weight
 plt.legend(prop={'family': 'Times New Roman', 'weight': 'bold', 'size': 40}, loc="upper left", frameon=False)
 plt.grid(ls='--')
 plt.tight_layout()
-# plt.savefig('ass_overall_and_minmax_{}_{}.pdf'.format(n, m), dpi=100)
+plt.savefig('ass_overall_and_minmax_{}_{}.pdf'.format(n, m), dpi=100)
 plt.show()
